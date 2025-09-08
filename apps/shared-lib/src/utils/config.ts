51ec5bdb0486d67aecabaef47de0cd24196cd2e1
ganjianping <ganjianping1984@gmail.com>
2025-09-09 05:42:55 +0700

    chore: commit latest changes

--- apps/shared-lib/src/utils/config.ts
+++ apps/shared-lib/src/utils/config.ts
@@ -6,7 +6,7 @@ export const APP_ENV = {
   DEV: import.meta.env.DEV,
   PROD: import.meta.env.PROD,
   MODE: import.meta.env.MODE ?? 'development',
-  VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL ?? '/api',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL ?? '/api',
 };
 
 export const APP_CONFIG = {
